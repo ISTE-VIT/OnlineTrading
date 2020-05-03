fb734a5619f7bc8181a06f607b9b2341a7462d5e
prateekbose <34328350+prateekbose@users.noreply.github.com>
2020-05-03 22:34:28 +0530

    Fixed Sidebad, Added Category Page
    -Reduced Width of Sidebar
    -Added Section Preview
    -Added Category Page

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const catogorySlide = () =>{
+const intViewportWidth = window.innerWidth;
+const intViewportHeight = window.innerHeight;
+
+const catogorySlide = () => {
     const burger = document.querySelector('.burger');
     const section = document.querySelector('.catogories');
-    const content = document.querySelector('.content');
+    const display = document.querySelector('.display');
 
-    burger.addEventListener('click', ()=>{
+    burger.addEventListener('click', () => {
         section.classList.toggle('catogories-active');
-        burger.classList.toggle('burger-active');
-        content.classList.toggle('hidden');
+        burger.classList.toggle('burger-active');        
+        if(intViewportWidth>intViewportHeight){
+            display.classList.remove('display-active');
+        }
     });
 }
 
-catogorySlide();
+const sectionDisplay = () => {
+    const catogory = document.querySelectorAll('.catogories-links li');
+    const display = document.querySelector('.display');
+
+    catogory.forEach((item) => {
+        item.addEventListener('click', () =>{
+            if(intViewportWidth>intViewportHeight){
+                display.classList.toggle('display-active');
+            }
+            console.log(item)
+        });
+    })
+
+}
+
+const start = () => {
+    catogorySlide();
+    sectionDisplay();
+}
+
+start();
