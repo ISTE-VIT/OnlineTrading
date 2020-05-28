788cc0d29a98af1b35d1eaf254791071c7f4bc3d
prateekbose <34328350+prateekbose@users.noreply.github.com>
2020-05-28 13:38:16 +0530

    Navbar and Carousel
    -Changed Navbar
    -Added Carousel

--- app.js
+++ app.js
@@ -22,19 +22,75 @@ const sectionDisplay = () => {
     const display = document.querySelector('.display');
 
     catogory.forEach((item) => {
-        item.addEventListener('click', () =>{
+        item.addEventListener('click', () => {
             if(intViewportWidth>intViewportHeight){
                 display.classList.toggle('display-active');
             }
-            console.log(item)
         });
     })
 
 }
 
+const navScroll = () => {
+    const nav = document.querySelector('nav');
+    const logo = document.querySelector('.logo');
+    const links = document.querySelector('.customer-section'); 
+    const burger = document.querySelectorAll('.burger-line');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >= 6*nav.offsetHeight){
+            nav.classList.add('nav-dark');
+            links.classList.add('customer-section-dark');
+            logo.classList.add('logo-dark');
+        } else {
+            nav.classList.remove('nav-dark');
+            links.classList.remove('customer-section-dark');
+            logo.classList.remove('logo-dark');
+        }
+    });
+}
+
+const carousel = () => {
+    const carousel = document.querySelector(".recent");
+    const card = carousel.querySelector(".recent .small-promo");
+    const leftButton = document.querySelector(".button-wrapper .slideLeft");
+    const rightButton = document.querySelector(".button-wrapper .slideRight");
+
+    const carouselWidth = carousel.offsetWidth;
+    const cardStyle = card.currentStyle || window.getComputedStyle(card)
+    const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+
+    const cardWidth = card.offsetWidth;
+
+    let offset = 0;
+    let  max = carouselWidth - 3*(cardWidth+cardMarginRight);
+
+    console.log(`Max = ${max}`);
+    leftButton.addEventListener("click", function() {
+    if (offset/(cardWidth + cardMarginRight) !== 0) {
+        offset += cardWidth + cardMarginRight;
+        carousel.scrollLeft -= cardWidth + cardMarginRight;
+        console.log(carousel.scrollLeft);
+        }
+    })
+    
+    rightButton.addEventListener("click", function() {
+    if (offset/(cardWidth + cardMarginRight) !== -5) {
+        offset += cardWidth + cardMarginRight;
+        carousel.scrollLeft += cardWidth + cardMarginRight;
+        console.log(carousel.scrollLeft);
+    }
+    })  
+    
+
+}
+
+
 const start = () => {
     catogorySlide();
     sectionDisplay();
+    navScroll();
+    carousel();
 }
 
 start();
